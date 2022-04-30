27b00ab172edc3c723b0119f8775083f5a9a4b24
tweinert <tait.weinert@outlook.com>
2022-04-30 15:42:23 +0930

    weather by location func

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const key = config.MY_API_TOKEN;
 
 async function getWeatherByLocation() {
+  const locationText = document.querySelector("input");
+  const location = locationText.value;
+
   const response = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=adelaide&appid=" + key
+    "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + key + "&units=metric"
   );
   const weatherData = await response.json();
+  
   console.log(weatherData);
 }
 
-getWeatherByLocation();
+const btn = document.querySelector("button");
+btn.addEventListener("click", getWeatherByLocation, false);
